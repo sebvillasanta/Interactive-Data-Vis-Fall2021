42fc9bb054ccd3c31dd582ba31b5e126fcd93f40
sebvillasanta <sebvillasanta@gmail.com>
2021-10-27 15:03:53 -0400

    draw area chart

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -7,19 +7,22 @@
  d3.csv('../data/owidCO2Data.csv', d => {
    return {
      year: new Date(+d.year, 0, 1),
-     country: d.country,
+     country: d.iso_code,
      co2_per_capita: +d.co2_per_capita
    }
  }).then(data => {
    console.log('data :>> ', data);
 
+   const col = data.filter(d => d.country === 'COL')
+   console.log(col)
+
    // SCALES
    const xScale = d3.scaleTime()
-     .domain(d3.extent(data, d => d.year))
+     .domain(d3.extent(col, d => d.year))
      .range([margin, width - margin])
 
    const yScale = d3.scaleLinear()
-     .domain(d3.extent(data, d => d.co2_per_capita))
+     .domain(d3.extent(col, d => d.co2_per_capita))
      .range([height - margin, margin])
 
    // CREATE SVG ELEMENT
@@ -30,29 +33,58 @@
 
    // BUILD AND CALL AXES
 
-   // LINE GENERATOR FUNCTION
+   const xAxis = d3.axisBottom()
+     .scale(xScale);
+   const xAxisTranslate = height - margin
 
-   const lineGen = d3.line()
+   svg.append('g')
+     .attr('transform', `translate(0, ${xAxisTranslate})`)
+     .call(xAxis);
+
+   const yAxis = d3.axisLeft()
+     .tickFormat(d3.format('~s'))
+     .scale(yScale);
+
+   svg.append('g')
+     .attr('transform', `translate(${margin}, 0)`)
+     .call(yAxis)
+
+   // AREA CHART 
+
+   const area = d3.area()
      .x(d => xScale(d.year))
-     .y(d => yScale(d.co2_per_capita))
+     .y1(d => yScale(d.co2_per_capita))
+     .y0(yScale(0))
 
-   const countriesMap = d3.group(data, d => d.country)
-   console.log('countriesMap :>> ', countriesMap);
+   // LINE GENERATOR FUNCTION
+   //  const lineGen = d3.line()
+   //    .x(d => xScale(d.year))
+   //    .y(d => yScale(d.co2_per_capita))
 
-   const countriesArray = Array.from(countriesMap)
-   console.log('countriesArray :>> ', countriesArray);
+   //  const countriesMap = d3.group(col, d => d.country)
+   //  console.log('countriesMap :>> ', countriesMap);
 
-   const countriesData = countriesArray.map(([key, data]) => data)
-   console.log('countriesData :>> ', countriesData);
+   //  const countriesArray = Array.from(countriesMap)
+   //  console.log('countriesArray :>> ', countriesArray);
+
+   //  const countriesData = countriesArray.map(([key, col]) => col)
+   //  console.log('countriesData :>> ', countriesData);
+
+   // DRAW AREA
+
+   svg.append('path')
+     .attr('d', area(col))
+     .attr('stroke', '#147F90')
+     .attr('stroke.width', '2px')
+     .attr('fill', '#A6E8F2')
 
    // DRAW LINE
-
-   svg.selectAll(".trend")
-     .data(countriesData)
-     .join("path")
-     .attr("class", "trend")
-     .attr("stroke", "black")
-     .attr("fill", "none")
-     .attr("d", d => lineGen(d))
+   //  svg.selectAll(".trend")
+   //    .data([col])
+   //    .join("path")
+   //  .attr("class", "trend")
+   //  .attr("stroke", "red")
+   //  .attr("fill", "none")
+   //  .attr("d", d => lineGen(d))
 
  });
